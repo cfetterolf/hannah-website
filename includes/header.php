e8845951d9de98fe21f138739b4d2a754906b577
cfetterolf <chris.fetterolf@gmail.com>
2017-08-23 23:38:05 -0700

    update

---
+++ includes/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>Hannah Green | Descibe yourself here</title>
+  <meta name="description" content="This is the home page of Chris Fetterolf, a software developer and student at Middlebury College.">
+  <meta name="author" content="Chris Fetterolf">
+
+  <!-- Icon -->
+  <link rel="icon" href="img/logo.png" type="image/png">
+
+  <!-- Open Graph - for social Media use -->
+  <meta prefix="og: http://ogp.me/ns#" property='og:title' content="Hannah Green - Student, Developer, Designer" />
+  <meta prefix="og: http://ogp.me/ns#" property='og:image' content="http://media.mydomain.com/thumbnail.jpg" />
+  <meta prefix="og: http://ogp.me/ns#" property="og:description" content="This is the home page of Hannah Green, a software developer and student at Middlebury College." />
+  <meta prefix="og: http://ogp.me/ns#" property="og:url" content="http://www.mydomain.com" />
+
+  <!-- FONT -->
+  <script src="https://use.typekit.net/dru0rgp.js"></script>
+  <script>try{Typekit.load({ async: true });}catch(e){}</script>
+  <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+  <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:300,400,500,600,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Fresca" rel="stylesheet">
+
+  <!-- To use: font-family: 'Zilla Slab', serif; -->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,500,700|Zilla+Slab:200,300,400,500,600,700" rel="stylesheet">
+  <!-- <script src="Tooltip.js" type="text/javascript"></script> -->
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/style.css">
+
+  <link rel="icon" type="image/png" href="img/favicon.png">
+
+</head>
+<body>
---
+++ includes/navbar-home.php
@@ -0,0 +1,35 @@
+<!-- Navbar -->
+<nav class="navbar navbar-toggleable-sm navbar-light bg-faded">
+  <button class="navbar-toggler navbar-toggler-right hover-click" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#"><strong>Hannah Green</strong></a>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#aboutMe">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#experience">Experience</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#resume">Resume</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#resume">Portfolio</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav mr-auto navbar-right">
+      <li class="nav-item">
+        <a class="nav-link blue" href="https://www.linkedin.com/in/chris-fetterolf-411b30ab/" target="_blank">in</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link blue" href="https://www.facebook.com/chris.fetterolf.7" target="_blank">fb</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link blue" href="https://www.facebook.com/chris.fetterolf.7" target="_blank">ig</a>
+      </li>
+    </ul>
+  </div>
+</nav> <!-- ./Navbar -->
